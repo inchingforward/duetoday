aa07036d5f7d9078b926852b4033586904d744db
mjanger <inchingforward@gmail.com>
2016-07-19 13:37:17 -0500

    Added stub webserver.

--- duetoday.go
+++ duetoday.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+)
+
+func index(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	e := echo.New()
+
+	e.GET("/", index)
+
+	log.Println("listening...")
+	e.Run(standard.New(":4001"))
 }
