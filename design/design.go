cd6dd564001b99c2739797bd1b84cf8aa0a7e93e
inchingforward <inchingforward@gmail.com>
2016-08-10 22:24:26 -0500

    Filled out more design.

--- design/design.go
+++ design/design.go
@@ -6,8 +6,39 @@ import (
 )
 
 var _ = API("duetoday", func() {
-		Title("Due Today")
-		Description("A todo service")
-		Scheme("http")
-		Host("localhost:4030")
-}
+	Title("Due Today")
+	Description("A todo service")
+	Scheme("http")
+	Host("localhost:4030")
+})
+
+var _ = Resource("todo", func() {
+	BasePath("/todos")
+	DefaultMedia(TodoMedia)
+
+	Action("show", func() {
+		Description("Get todo by id")
+		Routing(GET("/:todoID"))
+		Params(func() {
+			Param("todoID", Integer, "Todo ID")
+			Response(OK)
+			Response(NotFound)
+		})
+	})
+})
+
+// TodoMedia defines the media type used to render todos.
+var TodoMedia = MediaType("application/vnd.duetoday.todo+json", func() {
+	Description("A To Do")
+	Attributes(func() {
+		Attribute("id", Integer, "Unique Todo ID")
+		Attribute("title", String, "Todo title")
+		Attribute("details", String, "Additional notes")
+		Required("id", "title")
+	})
+	View("default", func() {
+		Attribute("id")
+		Attribute("title")
+		Attribute("details")
+	})
+})
